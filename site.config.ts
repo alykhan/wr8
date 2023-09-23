df0854438c5679f4c1b02587d8fb9f2941a75b75
alykhan <alykhanii@yahoo.com>
2023-09-23 12:52:13 -0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,14 +2,15 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7167d2cf133a4875b7e23caf545ee402',
+  // https://alykhan.notion.site/Planecrashdb-com-cd25bf259e254606b8a0d5f8a1007025?pvs=4
+  rootNotionPageId: 'cd25bf259e254606b8a0d5f8a1007025',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'wr8 Demo',
+  name: 'Plane Crash Database',
   domain: 'demo.wr8.in',
   author: 'Mighil',
   seotitle: 'Demo Site Created with wr8',
